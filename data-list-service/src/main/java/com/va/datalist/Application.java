3791dbdf8772acdb5f0f848a50dfb030d4d1408e
vaneet <vaneet.sharma1@yahoo.com>
2015-06-20 08:38:42 +0100

    new application class added

---
+++ data-list-service/src/main/java/com/va/datalist/Application.java
@@ -0,0 +1,18 @@
+package com.va.datalist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by hectar on 20/06/15.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+
+}
+
+
